351cad92a308ce195fba6b72b6a459fa8f7b54b2
v <v@DESKTOP-P4DFGP3>
2026-02-16 16:49:11 +0800

    Add project files.

---
+++ While loop/Program.cs
@@ -0,0 +1,27 @@
+﻿// The while loop
+
+Console.WriteLine("Enter go or stay");
+
+string userChoice = Console.ReadLine();
+
+if (userChoice.ToLower() == "stay")
+{
+    Console.WriteLine("Alright, take a good rest first ");
+}
+else if (userChoice.ToLower() == "go")
+{
+    while (userChoice.ToLower() == "go")
+    {
+        Console.WriteLine("Go walking for 10 km");
+        Console.WriteLine("Keep walking? Enter go again.");
+        userChoice = Console.ReadLine();
+    }
+
+    Console.WriteLine("Okay, now it's time to rest!");
+}
+else
+{
+    Console.WriteLine("Invalid input!");
+}
+
+Console.ReadKey();
